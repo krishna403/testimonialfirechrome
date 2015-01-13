964786495a8cb8241aa5ad0d9f764a57f53eebef
krishnapratap007 <krishnapratap101@gmail.com>
2015-01-13 12:29:36 +0530

    testing

--- delete.php
+++ delete.php
@@ -1,7 +1,4 @@
 <?php
-// Muaz Khan     - www.MuazKhan.com 
-// MIT License   - https://www.webrtc-experiment.com/licence/
-// Documentation - https://github.com/muaz-khan/WebRTC-Experiment/tree/master/RecordRTC
 if (isset($_POST['delete-file'])) {
     $fileName = 'uploads/'.$_POST['delete-file'];
     if(!unlink($fileName.'.webm') || !unlink($fileName.'.wav')) {
--- save.php
+++ save.php
@@ -1,7 +1,4 @@
 <?php
-// Muaz Khan     - www.MuazKhan.com 
-// MIT License   - https://www.webrtc-experiment.com/licence/
-// Documentation - https://github.com/muaz-khan/WebRTC-Experiment/tree/master/RecordRTC
 $count=0;
 //session_start();
 foreach(array('video', 'audio') as $type) {
@@ -19,50 +16,5 @@ foreach(array('video', 'audio') as $type) {
 		echo($fileName);
     }
  }
- /*
-$u_agent = $_SERVER['HTTP_USER_AGENT'];
-$ub = '';
-if(preg_match('/MSIE/i',$u_agent)){
-  $ub = "ie";}
-elseif(preg_match('/Firefox/i',$u_agent)){
-  $ub = "mozilla";}
-elseif(preg_match('/Chrome/i',$u_agent)){
-   $ub = "chrome";}
  
-   
- if($ub=='chrome'){  
- if(!empty($_POST["video-filename"])){
-     
-     $revitem= strrev($_POST["video-filename"]);
-  //   $char=substr( $str, 0, 4 );
-     $cropeditem= substr($revitem,4);
-     $file=  trim(strrev($cropeditem));
-     
-     
-   if(file_exists('uploads/'.$file.'webm') && file_exists('uploads/'.$file.'wav')){
-     
-     $audioFile='uploads/'.$file.'wav';
-     $videoFile='uploads/'.$file.'webm';
-                    
-      $mergedFile = 'uploads/'.$file.'webm-merged';
-      
-      echo "Starting ffmpeg...\n\n";
-     $merge= "-i $audioFile -i $videoFile 0:0 -map 1:0 $mergedFile"; 
-	exec('ffmpeg '.$merge.' 2>&1', $out, $ret);
-	echo "Done.\n";
-   
-     
-     
-     var_dump($merge);
-   //  exec('ffmpeg '.$merge.' 2>&1', $out, $ret);
-        // file_put_contents($mergedFile);
-        
-      //  if (!move_uploaded_file($mergedFile,'uploads/')) {
-      //      echo(" problem moving uploaded file");
-      //  }
-       // session_unset();
-   }
- }
- }  
-*/
 ?>
